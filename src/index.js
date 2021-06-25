807d18a346d71ade4155823c0bfe774c7bd5028a
Roan <roan@beetlebrain.nl>
2021-06-25 15:04:36 +0200

    clarity img

--- src/index.js
+++ src/index.js
@@ -4,18 +4,16 @@ import groundImage from "./assets/ground.png";
 import catSitImage from "./assets/cat-black-sit.png";
 import catWalkImage from "./assets/cat-black-walk.png";
 import platformImage from "./assets/platform.png";
+import clarityImage from "./assets/clarity_k52.png";
 
 class CatGame extends Phaser.Scene {
   constructor() {
     super();
     this.moveCam = false;
-    this.screenWidth = 736;
-    this.screenHeight = 414;
+    this.screenWidth = 552;
     this.screenSize = 4;
     this.player;
     this.platforms;
-    this.background;
-    this.message;
   }
 
   preload() {
@@ -25,16 +23,15 @@ class CatGame extends Phaser.Scene {
     this.load.image("cat-sit", catSitImage);
     this.load.image('ground', groundImage);
     this.load.image('platform', platformImage);
+    this.load.image('clarity', clarityImage);
   }
 
   create() {
-
     this.cameras.main.setBounds(0, 0, this.gameWidth, 176);
 
-  
-    this.background = this.add.tileSprite(0, 0, this.screenWidth, this.screenHeight, "bg")
-      .setOrigin(0)
-      .setScrollFactor(0,1);
+    for (let x = 0; x < this.screenSize; x++) {
+      this.add.image(552 * x, 0, "bg").setOrigin(0);
+    }
 
     this.platforms = this.physics.add.staticGroup();
     this.platforms.create(400, 250, 'platform');
@@ -54,23 +51,15 @@ class CatGame extends Phaser.Scene {
 
     // enemies
     this.enemies = this.physics.add.group({ allowGravity: true });
-    // this.enemies.create(550, 20, 'cat-sit');
-    // this.enemies.create(200, 250, 'cat-sit');
-
     this.enemies.add(new Enemy(this, 550, 20, 0.005), true);
     this.enemies.add(new Enemy(this, 200, 250, 0.005), true);
     this.physics.add.collider(this.enemies, this.platforms);
+
     this.physics.add.collider(this.enemies, this.player, () => {
         console.log("ouch!")
     });
-<<<<<<< HEAD
 
     // clarity code powerups
-=======
-    // this.enemy = this.physics.add.sprite(200, 150, 'cat-sit');
-    // this.enemy.setCollideWorldBounds(true);
-    // this.physics.add.collider(this.enemy, this.platforms);
->>>>>>> 64804f612d87686e9c7d38f493ab3732a74af04e
     this.clarityCodes = this.physics.add.group({ allowGravity: false });
     this.clarityCodes.add(new ClarityCode(this, 350, 100, 100, 100, 0.005), true);
     this.clarityCodes.add(new ClarityCode(this, 600, 200, 40, 100, 0.005), true);
@@ -80,39 +69,10 @@ class CatGame extends Phaser.Scene {
     this.physics.add.overlap(this.player, this.clarityCodes, function(player, code) {
         code.disableBody(true, true);
     }, null, this);
-    
-    this.message = this.add.text(150, 200, "Gefeliciteerd uren code opgepakt!", { font: "30px Arial", fill: "#ff0044",align: "center" }).setVisible(false);
-    this.message.setStroke('#000', 16);
-  }
-  
-  showMessage() {
-    this.message.setVisible(true);
-  }
-  
-  hideMessage() {
-    this.message.setVisible(false);  
   }
 
   update() {
-    this.background.setTilePosition(this.cameras.main.scrollX);
     const cursors = this.input.keyboard.createCursorKeys();
-
-    // this.enemies.children.each(enemy => {
-        
-    //     // if player to left of enemy AND enemy moving to right (or not moving)
-    //     if (550 <= enemy.x && enemy.body.velocity.x >= 0) {
-    //         // move enemy to left
-    //         enemy.body.velocity.x = -150;
-    //     }
-    //     // if player to right of enemy AND enemy moving to left (or not moving)
-    //     else if (250 >= enemy.x && enemy.body.velocity.x <= 0) {
-    //         // move enemy to right
-    //         enemy.body.velocity.x = 150;
-    //     }
-    // })
-    
-    
-
 
     if (this.moveCam) {
         this.player.setVelocityX(80);
@@ -122,22 +82,20 @@ class CatGame extends Phaser.Scene {
         this.moveCam = true;
         console.log("player x = ", this.player.x, " | gamewidth = ", this.gameWidth)
         this.player.setVelocityY(-250);
-        
+ 
     });
 
     if (cursors.left.isDown)
         {
             this.player.setVelocityX(0);
             this.player.setVelocityX(-160);
-            this.showMessage();
         }
-    else if (cursors.right.isDown)
+        else if (cursors.right.isDown)
         {
             this.player.setVelocityX(0);
             this.player.setVelocityX(160);
-            this.hideMessage();
         }
-    else if (cursors.up.isDown)
+        else if (cursors.up.isDown)
         {
             this.player.setVelocityY(0);
             this.player.setVelocityY(-160);
@@ -146,8 +104,6 @@ class CatGame extends Phaser.Scene {
             // this.player.setVelocity(0);
         }
      }
-
-
   }
 
 const config = {
@@ -177,7 +133,7 @@ const game = new Phaser.Game(config);
 var ClarityCode = new Phaser.Class({
     Extends: Phaser.Physics.Arcade.Sprite,
     initialize: function ClarityCode (scene, x, y, width, height, speed) {
-        Phaser.Physics.Arcade.Sprite.call(this, scene, x, y, 'cat-sit');
+        Phaser.Physics.Arcade.Sprite.call(this, scene, x, y, 'clarity');
 
         //  This is the path the sprite will follow
         this.path = new Phaser.Curves.Ellipse(x, y, width, height);
